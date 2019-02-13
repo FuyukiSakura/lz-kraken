6ad640b06a127318fcc952e55f72ba43789e5a3e
Eric Lam <eric.lam@qge.com.au>
2019-02-13 14:49:02 +1000

    initial push

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Copyright (c) UTOSOFT. All rights reserved.
+// Licensed under the MIT License.
+
+import { 海怪 } from './海怪';
+
+let 肉包 = new 海怪;
+console.log("肉包.fc(): " + 肉包.fc());
+console.log("肉包.ap(): " + 肉包.ap());
---
+++ src/海怪.ts
@@ -0,0 +1,15 @@
+export class 海怪 {
+    /**
+     * Gets if the kraken is able to Full Combo
+     */
+    public fc(){
+        return true;
+    }
+
+    /**
+     * Gets if the kraken is able to All Perfect
+     */
+    public ap(){
+        return true;
+    } 
+}
